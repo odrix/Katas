11bba49d40aa704cfc294f1f671c325e00dd5a38
odrix <adribey@free.fr>
2012-10-19 13:59:57 +0200

    3ème tests = 3 lancés

--- BowlingScoreTest/GameTest.cs
+++ BowlingScoreTest/GameTest.cs
@@ -9,7 +9,7 @@ namespace BowlingScoreTest
     public class GameTest
     {
         [Test]
-        public void GameOneRollKnockdown4Pins()
+        public void GameOneRollKnockdown4PinsScore0()
         {
             Game game = new Game();
             game.Roll(4);
@@ -18,11 +18,22 @@ namespace BowlingScoreTest
         }
 
         [Test]
-        public void Game2RollKnockdown7Pins()
+        public void Game2RollKnockdown4plus3PinsScore7()
         {
             Game game = new Game();
             game.Roll(4);
             game.Roll(3);
+
+            Assert.AreEqual(7, game.Score);
+        }
+
+        [Test]
+        public void Game3RollKnockdown4Plus3Plus8PinsScore7()
+        {
+            Game game = new Game();
+            game.Roll(4);
+            game.Roll(3);
+            game.Roll(8);
 
             Assert.AreEqual(7, game.Score);
         }
