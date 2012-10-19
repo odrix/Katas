cb27762e59b2552fb932b7ea1d05477181b01b0d
odrix <adribey@free.fr>
2012-10-19 14:05:46 +0200

    4ème test OK

--- BowlingScore/Game.cs
+++ BowlingScore/Game.cs
@@ -10,12 +10,23 @@ namespace BowlingScore
         private int _scoreTemporaire = 0;
         private int _nbRolls;
 
+        private int nbPinRoll1;
+        private int nbPinRoll2;
+
         public void Roll(int nbPins)
         {
             _scoreTemporaire += nbPins;
             _nbRolls++;
             if (_nbRolls % 2 == 0)
-                _score = _scoreTemporaire;
+            {
+                nbPinRoll2 = nbPins;
+                if(nbPinRoll1 + nbPinRoll2 != 10) // is Spare
+                    _score = _scoreTemporaire;
+            }
+            else
+            {
+                nbPinRoll1 = nbPins;
+            }
         }
 
         public int Score 
