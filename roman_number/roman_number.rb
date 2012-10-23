8ad2a633045a57d860cc02de3e16eb2f7717d757
adrip <adribey@free.fr>
2012-10-23 23:40:15 +0200

    test 5 success

--- roman_number/roman_number.rb
+++ roman_number/roman_number.rb
@@ -1,6 +1,27 @@
 class Roman_number
 	
-	def parse(s)
+	def parse(letters)
+		result = 0
+		letters.split(//).each_with_index do |num_char, index|
+			if(letters.length > index+1 && letters[index+1,1] == "V")
+				result -= parseOneLetter(num_char)
+			else
+				result += parseOneLetter(num_char)
+			end
+		end
+		return result
+	end
+
+	def parseOneLetter(letter)
+		if(letter == 'I')
+			return 1
+		end
+		if(letter == 'V')
+			return 5
+		end
+		if(letter == 'X')
+			return 10
+		end
 		return 0
 	end
 end
--- roman_number/testunit.rb
+++ roman_number/testunit.rb
@@ -15,5 +15,28 @@ class Tester < Test::Unit::TestCase
 		assert_equal(2, result)
 	end
 
+	def test_Vreturn5
+		rn = Roman_number.new()
+		result = rn.parse('V')
+		assert_equal(5, result)
+	end
+
+	def test_VIreturn6
+		rn = Roman_number.new()
+		result = rn.parse("VI")
+		assert_equal(6, result)
+	end
+
+	def test_IVreturn4
+		rn = Roman_number.new()
+		result = rn.parse("IV")
+		assert_equal(4, result)
+	end
+
+	def test_Xreturn10
+		rn = Roman_number.new()
+		result = rn.parse('X')
+		assert_equal(10, result)
+	end
 
 end
