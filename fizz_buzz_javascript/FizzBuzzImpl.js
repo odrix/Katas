c858bbb7154fc3b7d384107922fe3ffd84da02da
odrix <adribey@free.fr>
2012-11-27 16:26:39 +0100

    test jusqu'a "buzz" qui est en rouge

--- fizz_buzz_javascript/FizzBuzzImpl.js
+++ fizz_buzz_javascript/FizzBuzzImpl.js
@@ -1,5 +1,8 @@
 
 
 function DireNombre (_nombre) {
+	if(_nombre % 3 == 0)
+		return "Fizz";
+
 	return _nombre.toString();
 }
--- fizz_buzz_javascript/FizzBuzzTest.js
+++ fizz_buzz_javascript/FizzBuzzTest.js
@@ -1,18 +1,23 @@
 describe("Fizz Buzz - les Specs", function() {
 
   it("pour 1 dire 1", function() {
-    var result = DireNombre(1);
-    expect(result).toEqual("1");
+    expect(DireNombre(1)).toEqual("1");
   });
   
   it("pour 2 dire 2", function() {
-    var result = DireNombre(2);
-    expect(result).toEqual("2");
+    expect(DireNombre(2)).toEqual("2");
   });
   
-  it("pour 3 dire Buzz", function() {
-    var result = DireNombre(3);
-    expect(result).toEqual("Buzz");
+  it("pour 3 dire Fizz", function() {
+    expect(DireNombre(3)).toEqual("Fizz");
+  });
+  
+  it("pour 4 dire 4", function() {
+    expect(DireNombre(4)).toEqual("4");
+  });
+  
+  it("pour 5 dire Buzz", function() {
+    expect(DireNombre(4)).toEqual("Buzz");
   });
   
 });
