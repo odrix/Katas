e822276f2c63ccf71562eec3261019ec6892a9d8
odrix <adribey@free.fr>
2012-11-27 16:30:44 +0100

    test pour "Buzz" ok et test pour 15 ==> FizzBuzz NOK

--- fizz_buzz_javascript/FizzBuzzImpl.js
+++ fizz_buzz_javascript/FizzBuzzImpl.js
@@ -3,6 +3,9 @@
 function DireNombre (_nombre) {
 	if(_nombre % 3 == 0)
 		return "Fizz";
+		
+	if(_nombre % 5 == 0)
+		return "Buzz";
 
 	return _nombre.toString();
 }
--- fizz_buzz_javascript/FizzBuzzTest.js
+++ fizz_buzz_javascript/FizzBuzzTest.js
@@ -1,23 +1,47 @@
 describe("Fizz Buzz - les Specs", function() {
 
-  it("pour 1 dire 1", function() {
+  it("pour 1 dire  1", function() {
     expect(DireNombre(1)).toEqual("1");
   });
   
-  it("pour 2 dire 2", function() {
+  it("pour 2 dire  2", function() {
     expect(DireNombre(2)).toEqual("2");
   });
   
-  it("pour 3 dire Fizz", function() {
+  it("pour 3 dire  Fizz", function() {
     expect(DireNombre(3)).toEqual("Fizz");
   });
   
-  it("pour 4 dire 4", function() {
+  it("pour 4 dire  4", function() {
     expect(DireNombre(4)).toEqual("4");
   });
   
-  it("pour 5 dire Buzz", function() {
-    expect(DireNombre(4)).toEqual("Buzz");
+  it("pour 5 dire  Buzz", function() {
+    expect(DireNombre(5)).toEqual("Buzz");
+  });
+  
+  it("pour 6 dire  Fizz", function() {
+    expect(DireNombre(6)).toEqual("Fizz");
+  });
+  
+  it("pour 7 dire  7", function() {
+    expect(DireNombre(7)).toEqual("7");
+  });
+  
+  it("pour 8 dire  8", function() {
+    expect(DireNombre(7)).toEqual("7");
+  });
+  
+  it("pour 9 dire  Fizz", function() {
+    expect(DireNombre(9)).toEqual("Fizz");
+  });
+  
+  it("pour 10 dire Buzz", function() {
+    expect(DireNombre(10)).toEqual("Buzz");
+  });
+  
+  it("pour 15 dire FizzBuzz", function() {
+    expect(DireNombre(15)).toEqual("FizzBuzz");
   });
   
 });
