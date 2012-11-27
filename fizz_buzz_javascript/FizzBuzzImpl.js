fe4dbaeade9a8c7eaf9c15b8e8977ecd0961bc3d
odrix <adribey@free.fr>
2012-11-27 16:11:01 +0100

    début du kata FizzBuzz en javascript

---
+++ fizz_buzz_javascript/FizzBuzzImpl.js
@@ -0,0 +1,5 @@
+
+
+function DireNombre (_nombre) {
+	return "0";
+}
---
+++ fizz_buzz_javascript/FizzBuzzTest.js
@@ -0,0 +1,8 @@
+describe("Fizz Buzz - les Specs", function() {
+
+  it("Pour 1 dire 1", function() {
+    var result = DireNombre(1);
+    expect(result).toEqual("1");
+  });
+  
+});
