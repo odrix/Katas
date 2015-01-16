0fd69f21bf59481edeb0f4336439c7cea30b116e
Adrien <adribey@free.fr>
2015-01-16 17:17:16 +0100

    tentative de kata en F#

---
+++ score_bowling_fsharp/BowlingScore/Bowling.fs
@@ -0,0 +1,30 @@
+﻿module BowlingScore
+
+let IsSpare (lances: int[]) = lances.[0] + lances.[1] = 10
+
+let IsStrick (lances: int[]) = lances.[0] = 10
+
+let rec sum list indexPartie  =
+    match Array.length(list) with
+    | 0 | 1 -> 0
+    | _ -> 
+        
+        let total(partie, bonus, restant) = partie + bonus + sum restant (indexPartie+1)
+        let toutca = 
+            if IsStrick(list)  then
+                let partie = 10
+                let bonus = list.[1] + list.[2]
+                (partie, bonus, (if indexPartie < 9 then list.[1..]  else Array.empty))
+            else
+                let partie = list.[0] + list.[1]
+                let bonus = 
+                    match IsSpare(list)  with
+                    |true -> list.[2] 
+                    | _ -> 0
+                (partie, bonus, list.[2..])
+        total toutca
+
+
+let CalculScore lances = sum lances 0
+
+
---
+++ score_bowling_fsharp/testUnitaire/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace testUnitaire
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DeuxLancer4et3_score7()
+        {
+            var lances = new List<int>();
+            lances.Add(4);
+            lances.Add(3);
+            int score = BowlingScore.CalculScore(lances.ToArray());
+            Assert.AreEqual(7, score);
+        }
+
+        [TestMethod]
+        public void QuatreLancer4et3Puis2et2_score11()
+        {
+            int score = BowlingScore.CalculScore(new [] {4,3,2,2});
+            Assert.AreEqual(11, score);
+        }
+
+        [TestMethod]
+        public void SparePuis2et2_score16()
+        {
+            int score = BowlingScore.CalculScore(new [] { 5, 5, 2, 2 });
+            Assert.AreEqual(16, score);
+        }
+
+        [TestMethod]
+        public void DixSpareDeSuiteEt5_score168()
+        {
+            var lances = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                lances.Add(7);
+                lances.Add(3);
+            }
+            lances.Add(5);
+            int score = BowlingScore.CalculScore(lances.ToArray());
+            Assert.AreEqual(168, score);
+        }
+
+        [TestMethod]
+        public void StrickPuis5et3_score26()
+        {
+            int score = BowlingScore.CalculScore(new[] { 10, 5, 3});
+            Assert.AreEqual(26, score);
+        }
+
+        [TestMethod]
+        public void QueDesStrick_score300()
+        {
+            var lances = new List<int>();
+            for (int i = 0; i < 12; i++)
+            {
+                lances.Add(10);
+            }
+            int score = BowlingScore.CalculScore(lances.ToArray());
+            Assert.AreEqual(300, score);
+        }
+    }
+}
